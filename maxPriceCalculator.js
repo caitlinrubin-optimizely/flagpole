65a96fd5e7ecee59e52147b65343ffe2a6219704
Zachary Power <zachary.power@optimizely.com>
2018-12-13 21:07:25 -0800

    githubs api isnt working sos

--- maxPriceCalculator.js
+++ maxPriceCalculator.js
@@ -11,19 +11,22 @@ module.exports = (optlyInstance) => {
   let actualLandPrice = normalLandPrice;
   let actualSeaPrice = normalSeaPrice;
 
-  const isSeaPriceHikeActivated = optlyInstance.isFeatureEnabled('bikini_bottom_price_hike', userId, { location });
+  const isSeaPriceHikeActivated = optlyInstance
+    .isFeatureEnabled('bikini_bottom_price_hike', userId, { location });
   if (isSeaPriceHikeActivated) {
     actualSeaPrice += 100;
   }
 
-  const isLandPriceHikeActivated = optlyInstance.isFeatureEnabled('texas_price_hike', userId, { location });
+  const isLandPriceHikeActivated = optlyInstance
+    .isFeatureEnabled('texas_price_hike', userId, { location });
   if (isLandPriceHikeActivated) {
     actualLandPrice += 200;
   }
 
   const subtotal = location === 'land' ? actualLandPrice : actualSeaPrice;
 
-  const isSaleActivated = optlyInstance.isFeatureEnabled('sale_promo', userId, { location });
+  const isSaleActivated = optlyInstance
+    .isFeatureEnabled('sale_promo', userId, { location });
   if (isSaleActivated) {
     return `$${ Math.round(subtotal * 0.8) }.00 (20% off!)`;
   }
